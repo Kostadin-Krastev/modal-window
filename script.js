a886aa1e7334c240519730db6d33ba59cceef101
Kostadin-Krastev <kostadin.hariskov@gmail.com>
2025-05-09 20:46:47 +0300

    add Event listener touchstart for closing the modal window on mobile devices

--- script.js
+++ script.js
@@ -6,15 +6,22 @@ const showModalButton = document.querySelectorAll('.show-modal');
 const closeModalButton = document.querySelector('.close-modal');
 
 // Opening the modal window when showModalButton is clicked
+const openModalWindow = function () {
+  modalWindow.classList.remove('hidden');
+  overlayBackground.classList.remove('hidden');
+};
+
 for (let i = 0; i < showModalButton.length; i++) {
-  showModalButton[i].addEventListener('click', function () {
-    modalWindow.classList.remove('hidden');
-    overlayBackground.classList.remove('hidden');
-  });
+  showModalButton[i].addEventListener('click', openModalWindow);
 }
 
-// Closing the modal window when the X 'closeModalButton' button is clicked
-closeModalButton.addEventListener('click', function () {
+// Closing the modal window when the X 'closeModalButton' button is clicked or the space overlay around the modal window
+const closeModalWindow = function () {
   modalWindow.classList.add('hidden');
   overlayBackground.classList.add('hidden');
-});
+};
+
+closeModalButton.addEventListener('click', closeModalWindow);
+closeModalButton.addEventListener('touchstart', closeModalWindow);
+overlayBackground.addEventListener('click', closeModalWindow);
+overlayBackground.addEventListener('touchstart', closeModalWindow);
